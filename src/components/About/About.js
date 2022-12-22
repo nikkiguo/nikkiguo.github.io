d4cd25c413df9de62ac2cd2d5d4a36b6fd031239
Nikki <46271636+nikkiguo@users.noreply.github.com>
2022-12-22 08:00:44 -0500

    about updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './About.css';
-import "animate.css/animate.min.css";
+// import "animate.css/animate.min.css";
 import Fade from 'react-reveal/Fade';
 
 
@@ -20,10 +20,10 @@ function About() {
                     <div className="About-container-child">
                         <Fade duration={1500}>
                             <p className="About-description">
-                                Hi, I’m Nikki! I am a first year Computer Science student at the University of Waterloo. <br /><br />
+                                Hi, I’m Nikki! I am a second year Computer Science student at the University of Waterloo. <br /><br />
                                 As a software developer, I aim to create products that make a meaningful impact for users. <br /><br />
                                 Through past internships, side projects, and extracurriculars, I have attained skills in mobile/web development, data science, design, and leadership. <br /><br />
-                                In my free time, I like to draw, paint, read books, run, volunteer in my community, and explore the outdoors. <br /><br />
+                                In my free time, I enjoy drawing, painting, reading, working out, running, and exploring the outdoors! <br /><br />
                                 Feel free to <a className="About-link" href="https://www.linkedin.com/in/nikkiguo" target='_new'>reach out</a>! <br /><br />
                                 <a className="About-link" href="https://www.github.com/nikkiguo" target='_new'>GitHub</a> &nbsp;| &nbsp;
                                 <a className="About-link" href="https://www.linkedin.com/in/nikkiguo" target='_new'>LinkedIn</a> &nbsp;| &nbsp;
